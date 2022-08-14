45195c35d653595c07619564911b68f7bdc05b38
nikohonu <nikohonu@gmail.com>
2022-08-14 03:21:18 +0300

    start create user recommendation page add catppuccin.css colors add style to style.css add navbar to /user/<username> add sharable template for /user/<username> pages

--- game_lists_site/blueprints/user.py
+++ game_lists_site/blueprints/user.py
@@ -24,3 +24,23 @@ def user(username: str):
                                steam_profile=steam_profile, steam_profile_apps=steam_profile_apps)
     else:
         return abort(404)
+
+@bp.route('/<username>/recommendation')
+def recommendation(username: str):
+    db = get_db()
+    steam_profile_id = db.execute(
+        'SELECT steam_profile_id FROM user WHERE username = ?',
+        (username,)
+    ).fetchone()
+    if steam_profile_id:
+        steam_profile_id = steam_profile_id[0]
+    else:
+        abort(404)
+    steam_profile = get_profile(steam_profile_id)
+    steam_profile_apps = sorted(list(get_profile_apps(steam_profile_id)), key= lambda x: x.playtime, reverse=True)
+    if steam_profile:
+        return render_template('user/recommendation.html', username=username,
+                               steam_profile=steam_profile, steam_profile_apps=steam_profile_apps)
+    else:
+        return abort(404)
+
